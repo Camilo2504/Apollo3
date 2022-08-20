be369e24a4c00892042f2f1ddf3a7ca9f17560f4
supe <miguelsupe09@gmail.com>
2022-08-20 17:43:48 -0500

    commit miguel

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("1 prueba");
-        System.out.println("1 verificacion");
+        System.out.println(" miguel");
 
     }
 }
